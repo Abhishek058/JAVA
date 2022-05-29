21894ae89224f2f6221091c27cec143f4de05e02
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-29 23:34:49 +0530

    array sorting done

--- java_4.java
+++ java_4.java
@@ -1,17 +1,26 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class java_4 {
     public static void main(String[] args) {
         int rollNo = 43452;
-        int maths = 98;
-        int phy = 97;
+        int maths = 97;
+        int phy = 88;
         int chem = 87;
 
-        int[] studDetail = new int[3]; // arrays are non premetive data type
-        studDetail[0] = maths;
-        studDetail[1] = phy;
-        studDetail[2] = chem;
+        int[] studDetail = new int[4]; // arrays are non premetive data type
+        studDetail[0] = rollNo;
+        studDetail[1] = maths;
+        studDetail[2] = phy;
+        studDetail[3] = chem;
 
         System.out.println(studDetail); // some class code will be printed as output
 
-        
+        System.out.println(studDetail.length);
+
+        System.out.println(studDetail[1]);
+        Arrays.sort(studDetail);
+        //after sorting
+        System.out.println(studDetail[1]);
     }
 }
