d054b8426dabea40a529e4195e372903049293d8
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-29 15:14:35 +0530

    new file added for datatype | memory allocation of datatype

--- java_1.java
+++ java_1.java
@@ -1,5 +1,22 @@
+//Lecture 1 in which we will learn about giving out put
+// and about variables
+
 public class java_1 {
     public static void main(String[] args) {
-        System.out.println();
+        // Our first Program
+        System.out.println("Hello Java");
+        // sout snnipet for printing upper line
+
+        // Variables
+        String name = "Abhishek";
+        String nname = "Sujal";
+        int age = 30;
+
+        String friend = nname;
+
+        System.out.println(name);
+        System.out.println(nname);
+        System.out.println(age);
+        System.out.println(friend);
     }
 }
---
+++ java_2.java
@@ -0,0 +1,16 @@
+//Lecture 1 in which we will learn about DataType
+
+public class java_2 {
+    public static void main(String[] args) {
+
+        // byte -1 [-128 to 127] bytes
+        // short - 2 bytes
+        // int - 4 bytes
+        // long - 8 bytes
+        // float - 4 bytes
+        // double - 8 bytes
+        // char - 2 bytes
+        // boolean - 1 bytes
+
+    }
+}
