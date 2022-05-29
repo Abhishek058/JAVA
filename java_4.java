c5ca0290c0bd44bab50152399ed31f5e912a93d7
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-29 23:16:12 +0530

    array initialization done

---
+++ java_4.java
@@ -0,0 +1,17 @@
+public class java_4 {
+    public static void main(String[] args) {
+        int rollNo = 43452;
+        int maths = 98;
+        int phy = 97;
+        int chem = 87;
+
+        int[] studDetail = new int[3]; // arrays are non premetive data type
+        studDetail[0] = maths;
+        studDetail[1] = phy;
+        studDetail[2] = chem;
+
+        System.out.println(studDetail); // some class code will be printed as output
+
+        
+    }
+}
