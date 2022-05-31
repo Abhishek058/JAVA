dc569bc8b11d94a91daf40c77d8611db1b2b1038
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-31 23:05:48 +0530

    taking input || integer, string, sentence

---
+++ java_7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class java_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        /********Integer input*********/
+        // System.out.println("Input Integer: ");
+        // float age = sc.nextFloat();
+        // System.out.println(age);
+
+        /********String input*********/
+        // System.out.println("Input String: ");
+        // String name = sc.next();
+        // System.out.println(name);
+        
+        /********Sentence input*********/
+        System.out.println("Input Sentence: ");
+        String sentence = sc.nextLine();
+        System.out.println(sentence);
+    }
+}
