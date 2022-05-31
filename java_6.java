f6358e8a261eab613ce957deeb1230fb83953f99
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-31 22:17:21 +0530

    arithmetic operator added

---
+++ java_6.java
@@ -0,0 +1,17 @@
+//Lecture 6 in which we will learn about Operators
+
+public class java_6 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 3;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+    }
+}
