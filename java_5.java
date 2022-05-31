27d0d9b1385709ca5aa22e855290bf5b8e084377
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-31 22:11:09 +0530

    final keyword for constant value added

--- java_5.java
+++ java_5.java
@@ -11,8 +11,14 @@ public class java_5 {
         int fp = p + (int)18.78;  //explicit casting
         System.out.println(fp);
         
+        //Constants
         int age = 30;
         age = 31;
         age = 32;
+        System.out.println(age);
+
+        final float pi = 3.14F;  //This variable becomes constant by adding final keyword
+        // pi = 3.138F; //Error that we cannot change constant value
+        System.out.println(pi);
     }
 }
