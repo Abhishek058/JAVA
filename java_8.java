0ddb64ed984c1587c2b6a56d24a6500afe39c23b
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-31 23:38:05 +0530

    new file added || 3 max finding code added in java_8

--- java_8.java
+++ java_8.java
@@ -6,13 +6,32 @@ public class java_8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Input Age: ");
-        int age = sc.nextInt();
+        // System.out.println("Input Age: ");
+        // int age = sc.nextInt();
 
-        if (age >= 18) {
-            System.out.println("Can Vote");
-        } else {
-            System.out.println("Cannot Vote");
+        // if (age >= 18) {
+        //     System.out.println("Can Vote");
+        // } else {
+        //     System.out.println("Cannot Vote");
+        // }
+
+        System.out.println("Input A: ");
+        int a = sc.nextInt();
+        System.out.println("Input B: ");
+        int b = sc.nextInt();
+        System.out.println("Input C: ");
+        int c = sc.nextInt();
+
+        if(a>b && a>c){
+            System.out.println("A is max");
+        }
+        
+        else if(b>c && b>a){
+            System.out.println("B is max");
+        }
+        
+        else if(c>a && c>a){
+            System.out.println("C is max");
         }
     }
 }
---
+++ java_9.java
@@ -0,0 +1,5 @@
+public class java_9 {
+    public static void main(String[] args) {
+        
+    }
+}
