bed6b5eca3936eeb789a184e6ea9575ba041819e
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-06-02 22:05:10 +0530

    Function void to print added || simple code to try

---
+++ java_12.java
@@ -0,0 +1,12 @@
+//Lecture 12 in which we will learn about Function
+
+public class java_12 {
+    public static void print() {
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+        print();
+        print();
+        print();
+    }
+}
