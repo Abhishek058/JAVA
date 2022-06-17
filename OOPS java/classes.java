54353a3fd64cb119cf22b974bbff3747b83ed90e
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-06-17 17:44:57 +0530

    new folder added || Pen class added in new file

---
+++ OOPS java/classes.java
@@ -0,0 +1,15 @@
+class Pen {
+    String color;
+    String type;
+
+    public void write() {
+        System.out.println("Writing something");
+    }
+}
+
+public class classes {
+    public static void main(String[] args) {
+        Pen pen1 = new Pen();
+        
+    }
+}
