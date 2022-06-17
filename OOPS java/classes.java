57e3d862571140b201ae14328fa9025212341c02
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-06-17 17:56:31 +0530

    pen class element initialize and output printed

--- OOPS java/classes.java
+++ OOPS java/classes.java
@@ -5,11 +5,23 @@ class Pen {
     public void write() {
         System.out.println("Writing something");
     }
+
+    public void getcolor() {
+        System.out.println(color);
+    }
+
+    public void gettype() {
+        System.out.println(type);
+    }
 }
 
 public class classes {
     public static void main(String[] args) {
         Pen pen1 = new Pen();
-        
+        pen1.color = "blue";
+        pen1.type = "gel";
+        pen1.write();
+        pen1.getcolor();
+        pen1.gettype();
     }
 }
