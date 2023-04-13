34d2d20e1dd53a910d706f095d1b04e5def9fb27
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-04-13 14:19:39 +0530

    diamond pattern

---
+++ JAVA Chitkara/PATTERNS/pattern12.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class pattern12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int k = n - 1; k >= i; k--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int k = n - 1; k >= i; k--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        sc.close();
+    }
+}
