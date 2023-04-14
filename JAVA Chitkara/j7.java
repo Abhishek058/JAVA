66fa6c6dcda30a262fbf7197e2982028b4d97779
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-04-14 10:16:25 +0530

    012 sort using switch case

--- JAVA Chitkara/j7.java
+++ JAVA Chitkara/j7.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
+
 public class j7 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int zeroCount = 0, oneCount = 0, twoCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            switch (arr[i]) {
+                case 0:
+                    zeroCount++;
+                    break;
+                case 1:
+                    oneCount++;
+                    break;
+                case 2:
+                    twoCount++;
+                    break;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i < zeroCount) {
+                arr[i] = 0;
+            } else if (i < zeroCount + oneCount) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 2;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
 }
