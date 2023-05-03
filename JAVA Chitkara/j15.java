13de5f98e4db2507afa6df71adda6398a4640b7f
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-05-03 11:50:02 +0530

    recursion practice question

--- JAVA Chitkara/j15.java
+++ JAVA Chitkara/j15.java
@@ -75,14 +75,77 @@ public class j15 {
 
     }
 
+    public static int first = -1;
+    public static int last = -1;
+
+    public static void firstLastOcc(String str, int idx, char ch) {
+        if (idx == str.length()) {
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+
+        char currCh = str.charAt(idx);
+        if (currCh == ch) {
+            if (first == -1) {
+                first = idx;
+            } else {
+                last = idx;
+            }
+        }
+        firstLastOcc(str, idx + 1, ch);
+    }
+
+    public static boolean isSorted(int arr[], int idx) {
+        if (idx == arr.length - 1) {
+            return true;
+        }
+        if (arr[idx] >= arr[idx + 1]) {
+            return false;
+        }
+        return isSorted(arr, idx + 1);
+    }
+
+    public static void moveAllX(String str, int idx, int cnt, String st) {
+        if (idx == str.length()) {
+            for (int i = 0; i < cnt; i++) {
+                st += 'x';
+            }
+            System.out.println(st);
+            return;
+        }
+        char currCh = str.charAt(idx);
+        if (currCh == 'x') {
+            cnt++;
+            moveAllX(str, idx + 1, cnt, st);
+        } else {
+            st += currCh;
+            moveAllX(str, idx + 1, cnt, st);
+        }
+    }
+
     public static void main(String[] args) {
         // fernheit();
+
         // printNum(6);
+
         // System.out.println(sumNum(4, 5));
+
         // System.out.println(factorial(20));
+
         // System.out.println(fibonnaci(6));
+
         // System.out.println(power(2, 10));
+
         // towerOfHanoi(3, "S", "H", "D");
+
         // reverseString("abhishek", 7);
+
+        // firstLastOcc("babaabbcacb", 0, 'a');
+
+        // int arr[] = { 1, 3, 5, 5 };
+        // System.out.println(isSorted(arr, 0));
+
+        // moveAllX("bxdxxcbcxcb", 0, 0, "");
     }
 }
