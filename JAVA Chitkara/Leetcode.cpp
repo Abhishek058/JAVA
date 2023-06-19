805664cb96299e350487f496b3e9359fd6dd24e4
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-06-19 14:27:42 +0530

    codes updated

--- JAVA Chitkara/Leetcode.cpp
+++ JAVA Chitkara/Leetcode.cpp
@@ -590,30 +590,41 @@ public:
     {
         int n = nums.size();
 
-        for (int i = 0; i < n; i++)
+        auto it = find(nums.begin(), nums.end(), 1);
+        if (it == nums.end())
         {
-            if (nums[i] <= 0)
-                nums[i] = n + 1;
+            return 1;
         }
+        sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < n; i++)
-        {
-            if (abs(nums[i]) <= n && nums[abs(nums[i]) - 1] > 0)
-                nums[abs(nums[i]) - 1] *= -1;
-        }
-
+        int idx;
         for (int i = 0; i < n; i++)
         {
             if (nums[i] > 0)
-                return i + 1;
+            {
+                idx = i;
+                break;
+            }
         }
-        return n + 1;
+        int j = 0;
+        for (int i = idx; i < n - 1; i++)
+        {
+            if (nums[i] != ++j)
+            {
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return j;
     }
 };
 
 int main()
 {
     Solution s;
-    vector<int> nums = {0, 1, 2};
+    vector<int> nums = {3, 4, -1, 1};
     cout << s.firstMissingPositive(nums);
 }
